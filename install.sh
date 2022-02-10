5a91af65fc48c062f5c318dc874eb678de696eec
akutruff <874049+akutruff@users.noreply.github.com>
2022-02-10 18:33:05 +0000

    add username detection and require typescript-node

--- install.sh
+++ install.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
 set -e
 
+# Determine the appropriate non-root user
+if [ "${USERNAME}" = "auto" ] || [ "${USERNAME}" = "automatic" ]; then
+    USERNAME=""
+    POSSIBLE_USERS=("vscode" "node" "codespace" "$(awk -v val=1000 -F ":" '$3==val{print $1}' /etc/passwd)")
+    for CURRENT_USER in ${POSSIBLE_USERS[@]}; do
+        if id -u ${CURRENT_USER} > /dev/null 2>&1; then
+            USERNAME=${CURRENT_USER}
+            break
+        fi
+    done
+    if [ "${USERNAME}" = "" ]; then
+        USERNAME=root
+    fi
+elif [ "${USERNAME}" = "none" ] || ! id -u ${USERNAME} > /dev/null 2>&1; then
+    USERNAME=root
+fi
+
 # The install.sh script is the installation entrypoint for any dev container 'features' in this repository. 
 #
 # The tooling will parse the devcontainer-features.json + user devcontainer, and write 
